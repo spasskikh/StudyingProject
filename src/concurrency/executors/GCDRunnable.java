de84902fa1b83548c42ea3e1a4e6b0f01e803f52
Anton_Spasskikh <spasskikh@gmail.com>
2019-02-13 17:14:26 +0200

    executors - minor updates

--- src/concurrency/executors/GCDRunnable.java
+++ src/concurrency/executors/GCDRunnable.java
@@ -5,12 +5,16 @@ import java.util.Random;
 /*GREATEST COMMON DIVIDER*/
 public class GCDRunnable extends Random implements Runnable {
 
-    public GCDRunnable() {
+    private boolean isDaemon;
+
+    GCDRunnable(boolean isDaemon) {
+        this.isDaemon = isDaemon;
     }
 
     @Override
     public void run() {
-        String threadDescription = Thread.currentThread().getName();
+        String threadType = isDaemon ? "- daemon - " : "- user - ";
+        String threadDescription = threadType + Thread.currentThread().getName();
         System.out.println("Starting " + threadDescription);
 
         for (int i = 0; i < 10_000_000; i++) {
@@ -18,9 +22,14 @@ public class GCDRunnable extends Random implements Runnable {
             int b = nextInt();
 
             if (i % 10_000 == 0) {
-                int gcd = computeGCD(a, b);
-                if (gcd > 5) {
-                    System.out.println("Running in " + threadDescription + ". GCD of " + a + "and " + b + " is " + gcd);
+                if (!Thread.interrupted()) {
+                    int gcd = computeGCD(a, b);
+                    if (gcd > 5) {
+                        System.out.println("Running in " + threadDescription + ". GCD of " + a + " and " + b + " is " + gcd);
+                    }
+                } else {
+                    System.out.println("Thread is interrupted!");
+                    return;
                 }
             }
         }
@@ -28,8 +37,8 @@ public class GCDRunnable extends Random implements Runnable {
     }
 
     private int computeGCD(int number1, int number2) {
-        if (number1 == 0) {
-            return number2;
+        if (number2 == 0) {
+            return number1;
         } else {
             return computeGCD(number2, number1 % number2);
         }
--- src/concurrency/executors/Launcher.java
+++ src/concurrency/executors/Launcher.java
@@ -1,14 +1,55 @@
 package concurrency.executors;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 public class Launcher {
 
     private static final int POOL_SIZE = 2;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        boolean isDaemon = true;
+
         System.out.println("Starting MAIN thread");
+        GCDRunnable runnable = new GCDRunnable(isDaemon);
 
-        GCDRunnable r = new GCDRunnable();
+//        runInOneThread(runnable, isDaemon);
 
+        runWithExecutors(runnable, isDaemon);
+        Thread.sleep(1000);
         System.out.println("Leaving MAIN thread");
+
+    }
+
+    private static void runInOneThread(GCDRunnable runnable, boolean isDaemon) throws InterruptedException {
+        Thread thread = new Thread(runnable);
+        if (isDaemon) {
+            thread.setDaemon(true);
+        }
+        thread.start();
+        Thread.sleep(10);
+//        thread.interrupt();
+    }
+
+    private static void runWithExecutors(GCDRunnable runnable, boolean isDaemon) throws InterruptedException {
+
+        ThreadFactory factory = new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread thread = new Thread(r);
+                if (isDaemon) {
+                    thread.setDaemon(true);
+                }
+                return thread;
+            }
+        };
+
+        ExecutorService executor = Executors.newFixedThreadPool(POOL_SIZE, factory);
+        for (int i = 0; i < 5; i++) {
+            executor.execute(runnable);
+        }
+        executor.shutdown();
     }
 }
